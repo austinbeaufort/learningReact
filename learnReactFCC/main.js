2d68bb3603826b58ae9c7e363c03d4daa08a6048
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-19 18:28:03 -0400

    initial commit: learning React Basics freeCodeCamp Tutorial on Scrimba

---
+++ learnReactFCC/main.js
@@ -0,0 +1,58 @@
+// React Notes and Coding Exercises from https://scrimba.com/g/glearnreact 
+
+
+// *** JSX ***
+
+// Exercise 1, Render an unordered list with 3 li elements
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<ul>
+//                     <li>cat</li>
+//                     <li>dog</li>
+//                     <li>fish</li>
+//                 </ul>,
+//                 document.querySelector('#root'));
+
+
+// *** FUNCTIONAL COMPONENTS ***
+
+// function MyApp() {
+//     return  (
+//         <ul>
+//             <li>cat</li>
+//             <li>dog</li>
+//             <li>fish</li>
+//         </ul>
+//     )
+// }
+
+// ReactDOM.render(
+//     <MyApp />,
+//     document.querySelector('#root')
+// )
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function MyInfo() {
+//   return (
+//     <div>
+//       <h1 style={{color: 'green'}}>Austin Beaufort</h1>
+//       <p> Hi I'm Austin, a Web Developer from Cleveland, Ohio!!</p>
+//       <h3 style={{color: 'green'}}> Top 3 Places to Visit </h3>
+//       <ul>
+//         <li> Paris, France </li>
+//         <li> London, UK </li>
+//         <li> Grand Canyon, AZ </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <MyInfo />,
+//   document.querySelector('#root')
+//   );

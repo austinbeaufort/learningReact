c5ac5dbee35a3e7c825bb1249dc57f614064fd69
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-21 19:19:55 -0400

    Initial Commit: React Complete Guide Udemy Course

---
+++ completeGuideReact/notesExtraCodeExamples.js
@@ -0,0 +1,67 @@
+// 1st React Example I completed on Codepen, link below
+// https://codepen.io/tinyhatcoding/pen/oVQPGP
+
+
+// JAVASCRIPT REFRESHER
+// const printMyName = name => {
+//     console.log(name);
+// }
+
+
+// ARROW FUNCTIONS
+// const printMyName = name => console.log(name);
+
+// printMyName('Max');
+
+// const multiply = number => number * 2;
+
+// console.log(multiply(2));
+
+
+// EXPORTS AND IMPORTS (MODULES)
+
+// CLASSES 
+
+// class Human {
+//     gender = 'male';
+
+//     printGender = () => {
+//         console.log(this.gender);
+//     }
+// }
+
+
+// class Person extends Human {
+//     name = 'Austin';
+
+//     printMyName = () => {
+//         console.log(this.name);
+//     }
+// }
+
+// const person = new Person();
+// person.printMyName();
+// person.printGender();
+
+// CONSTRUCTOR FUNCTION ES6
+// constructor() {
+//     this.myProperty = 'value'
+// }
+
+// ES7 VERSION
+// myProperty = 'value'
+
+
+
+
+// SPREAD / REST OPERATORS
+
+// SPREAD => USED TO SPLIT UP ARRAY ELEMENTS OR OBJECT PROPERTIES.
+// const newArray = [...oldArray, 1, 2];
+// const newObject = {...oldObject, newProp: 5};
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers);
+
+// REST => USED TO MERGE A LIST OF FUNCTION ARGUMENTS INTO AN ARRAY.

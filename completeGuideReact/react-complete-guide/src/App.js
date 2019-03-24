cc2ea6b5bf7356b5df084c663c2d8fa3b42733df
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-24 11:18:49 -0400

    making content dynamic

--- completeGuideReact/react-complete-guide/src/App.js
+++ completeGuideReact/react-complete-guide/src/App.js
@@ -5,26 +5,35 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Austin', age: 26 },
-      { name: 'Max', age: 28 },
-      { name: 'Stephanie', age: 29 }
+      { id: '1', name: 'Austin', age: 26 },
+      { id: '2', name: 'Max', age: 28 },
+      { id: '3', name: 'Stephanie', age: 29 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
 
-  nameChangedHandler = event => {
-    this.setState({
-        persons: [
-          { name: 'Austin', age: 26 },
-          { name: event.target.value, age: 28 },
-          { name: 'Stephanie', age: 29 }
-        ]
-      })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
   }
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
@@ -52,10 +61,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <Person 
               click={() => this.deletePersonHandler(index)} 
               name={person.name} 
-              age={person.age}/>
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );

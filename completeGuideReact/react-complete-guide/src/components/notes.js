21331287ce190b86dfd02218ba9a5801f42970f4
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-26 18:16:31 -0400

    Debugging, Styling, Organizing Components Notes

---
+++ completeGuideReact/react-complete-guide/src/components/notes.js
@@ -0,0 +1,13 @@
+// NOTES
+
+// CLASS VS. FUNCTIONAL BASED COMPONENTS
+
+// CLASS BASED
+// class XY extends Component
+// Access to State
+// Lifecycle Hooks
+
+// FUNCTIONAL
+// const XY = props => {...}
+// Access to State (useState())
+// Lifecycle Hooks

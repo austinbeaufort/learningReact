5ea53c5f43589c3c48ad8d0fcc4640321fb18d4e
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-25 22:25:17 -0400

    completed assignment2

--- completeGuideReact/assignment2/src/App.js
+++ completeGuideReact/assignment2/src/App.js
@@ -14,8 +14,23 @@ inputChangedHandler = event => {
   this.setState({userInput: event.target.value})
 }
 
+deleteCharHandler = index => {
+  const text = this.state.userInput.split('');
+  text.splice(index, 1);
+  const updatedText = text.join('');
+  this.setState({userInput: updatedText});
+}
+
 render() {
 
+  const charList = this.state.userInput.split('').map((ch, index) => {
+    return (
+      <Char 
+      character={ch} 
+      key={index} 
+      clicked={() => this.deleteCharHandler(index)}/>
+    )
+  });
 
   return (
     <div className="App">
@@ -24,6 +39,7 @@ render() {
       value={this.state.userInput} />
       <p>{this.state.userInput}</p>
       <Validation inputLength={this.state.userInput.length} />
+      {charList}
     </div>
   )
 }
